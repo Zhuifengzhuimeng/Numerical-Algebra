4afbc271d941d130e7731ee2adfbda80ff4e702d
Jinze Wu <1700010643@qq.com>
2018-10-07 12:57:09 +0800

    report

--- Assignment2/cac1.m
+++ Assignment2/cac1.m
@@ -1,5 +1,8 @@
-
-for n=92:99
+T1=zeros(99,1);
+T2=zeros(99,1);
+T3=zeros(99,1);
+T4=zeros(99,1);
+for n=9:99
     x=sin([1/n:1/n:1-1/n]*pi);
     y=sin([1/n:1/n:1-1/n]*pi);
     f=2*pi^2*kron(x,y)';
@@ -10,14 +13,14 @@ A1=eye(n-1)*4+full(spdiags([v v],[-1,1],n-1,n-1))*(-1);
 A=kron(eye(n-1),A1);
 A=A+full(spdiags([u u],[-n+1,n-1],N,N))*(-1);
 A=A*n^2;
-%     t1=clock;
-%     r0=LUsolve1(A,f);
-%     t2=clock;
-%     T1(n)=etime(t2,t1);
-%     t1=clock;
-%     r0=squareLDL(A,f);
-%     t2=clock;
-%     T2(n)=etime(t2,t1);
+    t1=clock;
+    r0=LUsolve1(A,f);
+    t2=clock;
+    T1(n)=etime(t2,t1);
+    t1=clock;
+    r0=squareLDL(A,f);
+    t2=clock;
+    T2(n)=etime(t2,t1);
     t1=clock;
     r0=gauss_possion(n,f);
     t2=clock;
@@ -26,5 +29,4 @@ A=A*n^2;
     r0=A\f;
     t2=clock;
     T4(n)=etime(t2,t1);
-    n
 end
